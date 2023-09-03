d5e061649fc71c5f3f942f365a1c5f982e9ee48b
Anthony Tossell <atossell91@gmail.com>
2023-09-03 15:54:22 -0700

    A few changes

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,29 @@
 
 #include "ArgParser.h"
 
+void debug_printInfo(AntSpace::ArgData data);
+
 int main(int argc, const char* argv[]) {
     AntSpace::ArgParser parser;
 
     parser.AddSubCommandFunction([&](AntSpace::ArgData data){
-        std::cout << "The sub command is running" << std::endl;
+        std::cout << "The sub command ('" << data.Name << "') is running" << std::endl;
     });
 
-    parser.EnableSubCommand();
+    parser.DisableSubCommand();
 
-    parser.AddAction("foo", [&](AntSpace::ArgData d){});
-    parser.AddAction("bar", [&](AntSpace::ArgData d){});
+    parser.AddAction("name", debug_printInfo);
+    parser.AddAction("bar", debug_printInfo);
 
     parser.ParseArgs(argc, argv);
 
     return 0;
+}
+
+void debug_printInfo(AntSpace::ArgData data) {
+    std::cout << "Name is: " << data.Name << std::endl;
+    for (auto str : data.Data) {
+        std::cout << str << std::endl;
+    }
+    std::cout << "-----  -----  -----" << std::endl << std::endl;
 }
